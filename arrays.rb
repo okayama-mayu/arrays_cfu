86ed0801f3767d341c0d7aa66ac18e071522e109
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-04-20 10:47:53 -0500

    Add 4 Arrays

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+# Declare an Array of Strings
+names = ["Carlos", "Rosa", "Elaina", "Stefen"]
+
+# Declare an Array of Integers
+ages = [31, 70, 45, 27]
+
+# Declare an Array of Floats
+float_nums = [1.45, 0.56, 1.2, 0.45]
+
+# Declare an Array of Booleans
+liberals = [True, False, False, False]
