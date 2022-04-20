9301c88d6bb8bda01c0b077ff0d19a7b5f098dcf
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-04-20 11:30:29 -0500

    Add methods

--- arrays.rb
+++ arrays.rb
@@ -1,11 +1,23 @@
 # Declare an Array of Strings
 names = ["Carlos", "Rosa", "Elaina", "Stefen"]
 
+# Remove the last element "Stefen" using the pop method
+names.pop
+
 # Declare an Array of Integers
 ages = [31, 70, 45, 27]
+
+# Add an element "40" to the end of the array
+ages.push(40)
 
 # Declare an Array of Floats
 float_nums = [1.45, 0.56, 1.2, 0.45]
 
+# Remove the first element 1.45 from the Array
+flat_nums.shift
+
 # Declare an Array of Booleans
 liberals = [True, False, False, False]
+
+# Add an element "True" at the beginning of the array
+liberals.unshift(True)
